035693d4d9cb05b1bc2ded01776e2e31a541926e
Ryo Nakamura <ryo-nakamura@cookpad.com>
2014-01-16 16:33:22 +0900

    Update view

--- lib/presentations/view/presentations.rb
+++ lib/presentations/view/presentations.rb
@@ -8,11 +8,7 @@ module Presentations
       def render
         @presentations.sort_by(&:date).reverse.inject("") do |result, presentation|
           result << <<-EOS.gsub(/^ {10}/, "")
-            <li data-date="#{presentation.date}" class="cookpad-slides-element">
-              <a target="_blank" href="#{presentation.url}">
-                <img class="cookpad-slides-image" src="#{presentation.image_url}" />
-              </a>
-            </li>
+            <li data-date="#{presentation.date}" class="cookpad-slides-element"><a target="_blank" href="#{presentation.url}"><img class="cookpad-slides-image" src="#{presentation.image_url}" /></a></li>
           EOS
         end
       end
